430d457366c42667a7bad8c1f004d6fb81c198bf
46maru <c0b2108596@edu.teu.ac.jp>
2024-10-06 09:14:19 +0000

    deploy 修正

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import environ # type: ignore
 import dj_database_url # type: ignore
+import os # type: ignore
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -95,9 +96,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 # }
 
 DATABASES = {
-    'default': {
-        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 DATABASES['default']['OPTIONS'] = {'sslmode': 'require'}
